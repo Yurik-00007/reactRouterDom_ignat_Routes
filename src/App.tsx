2c7c3d1b972db4f8a6275907ac98e91a9e0fe166
Timofeev Yury <yurik-007@mail.ru>
2024-01-14 03:51:23 +0300

    Navigate,useNavigate,navigate(-1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {NavLink, Route, Routes, useNavigate} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes, useNavigate} from "react-router-dom";
 
-const Profile=()=>{
-    const navigate=useNavigate()
-    return(
+const Profile = () => {
+    const navigate = useNavigate()
+/*
+    useEffect(() => {
+        if (true) navigate('/login')
+
+    }, []);
+*/
+    return (
         <div>
-            provile
-            <button onClick={()=>{navigate('/login')}}>logout</button>
+            {/*1.//будет срабатывать дерганье сначало на profile затем на login*/}
+            {/*
+            {true && <Navigate to={'/login'}/>}
+            profile
+            <button onClick={() => {
+                navigate('/login')
+            }}>logout</button>
+*/}
+            {/*2.//не будет дерганье при тернарнике сразу на login*/}
+{/*
+            {true
+                ? <Navigate to={'/login'}/>
+                :<>
+                    profile
+                    <button onClick={() => {
+                        navigate('/login')
+                    }}>logout</button></>}
+*/}
+            {/*3.//у navigate может быть не только путь но и
+            числовое значение которое возвращает педидущий вкладку(-1)
+            так же может быть положительное(2)*/}
+
+            profile
+            <button onClick={() => {
+                navigate(1)
+            }}>logout</button>
+
+
+            {/*не сработало*/}
+            {/*<button onClick={()=><Navigate to={'/login'}/>}>logout</button>*/}
         </div>
     )
 }
@@ -31,7 +65,10 @@ function App() {
 
             <div className={'context'}>
                 <Routes>
-                    <Route path={'/*'} element={<div>404</div>}/>
+                    <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+
+                    <Route path={'/*'} element={<Navigate to={'/404'}/>}/>
+
                     <Route path={'/'} element={<div>main</div>}/>
                     <Route path={'/login'} element={<div>login</div>}/>
                     <Route path={'/profile'} element={<Profile/>}/>
